cf21b3432146c5b93499eeea7cf546bebdbf0e68
k <k>
2020-10-09 14:22:06 +0200

    Laboratorium 1. - Zapoznanie się z frameworkiem MVC (update)

--- mvc/Controllers/TestController.cs
+++ mvc/Controllers/TestController.cs
@@ -32,15 +32,7 @@ namespace mvc.Controllers
 
         public IActionResult TestModel() {
 
-            var model = new List<TestModel>
-            {
-                new TestModel { Name = "Product 1", Desc = "Descrition 1", Price = 1 },
-                new TestModel { Name = "Product 2", Desc = "Descrition 2", Price = 2 },
-                new TestModel { Name = "Product 3", Desc = "Descrition 3", Price = 3 },
-                new TestModel { Name = "Product 4", Desc = "Descrition 4", Price = 4 }
-
-
-            };
+            var model = repository.GetItems();
 
             return View(model);
         }
